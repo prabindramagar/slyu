28a385f60ed1ba2606c44601b00b1cd03f57445b
Prabindramagar <prabindramagar@gmail.com>
2021-09-01 16:12:00 +0545

    c

---
+++ backend/data.js
@@ -0,0 +1,110 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Puma T-shirt',
+            category: 'Shirts',
+            image: '/images/product-1.jpg',
+            price: 1200,
+            countInStock: 5,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '2',
+            name: 'HRX Shoe',
+            category: 'Shoes',
+            image: '/images/product-2.jpg',
+            price: 2500,
+            countInStock: 10,
+            brand: 'HRX',
+            rating: 4,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '3',
+            name: 'Gray Jogger',
+            category: 'Joggers',
+            image: '/images/product-3.jpg',
+            price: 1400,
+            countInStock: 0,
+            brand: 'Epsion',
+            rating: 4.5,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '4',
+            name: 'Puma blue T-shirt',
+            category: 'Shirts',
+            image: '/images/product-4.jpg',
+            price: 1000,
+            countInStock: 10,
+            brand: 'Puma',
+            rating: 3.5,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '5',
+            name: 'Gray Shoe',
+            category: 'Shoes',
+            image: '/images/product-5.jpg',
+            price: 2300,
+            countInStock: 0,
+            brand: 'Puma',
+            rating: 5,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '6',
+            name: 'Black T-shirt',
+            category: 'Shirts',
+            image: '/images/product-6.jpg',
+            price: 1200,
+            countInStock: 5,
+            brand: 'Puma',
+            rating: 4,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '7',
+            name: 'HRX Shocks',
+            category: 'Shocks',
+            image: '/images/product-7.jpg',
+            price: 800,
+            countInStock: 10,
+            brand: 'HRX',
+            rating: 3,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+        {
+            _id: '8',
+            name: 'Fossil Watch',
+            category: 'Watches',
+            image: '/images/product-8.jpg',
+            price: 12000,
+            countInStock: 0,
+            brand: 'Fossil',
+            rating: 5,
+            numReviews:10,
+            description: 'high quality product',
+        },
+
+    ]
+}
+
+export default data
