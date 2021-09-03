c52cb8daba2ab678f0d8d929f4c52ac560160787
Prabindramagar <prabindramagar@gmail.com>
2021-09-03 19:38:03 +0545

    c

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -21,7 +21,7 @@ productRouter.get('/seed', expressAsyncHandler(async(req, res) => {
 productRouter.get('/:id', expressAsyncHandler(async(req,res) => {
     const product = await Product.findById(req.params.id);
     if(product) {
-        res.send(products);
+        res.send(product);
     }
     else {
         res.status(404).send({message:'Product not Found'});
